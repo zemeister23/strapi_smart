77875affd9313804d7429bb6bded66f171f4045e
zemeister23 <sauda2332@gmail.com>
2022-01-04 22:35:46 +0500

    for digital

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9f21e74c1c21927b7ac3b078ff440117'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c41d88b6-efdf-4211-915b-4ffeb67d239b'
+};
